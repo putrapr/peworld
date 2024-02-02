7bf513c25bccba45dc5a958c7359430050f845c0
Putra Prasetya <putraprasetya333@gmail.com>
2024-02-02 15:16:54 +0700

    Add Hire Page

--- components/base/Skill/index.js
+++ components/base/Skill/index.js
@@ -2,12 +2,8 @@ import React from 'react'
 
 const index = ({ className, children }) => {
   return (
-    <>
-    <div className={`inline-block rounded border-2 border-[#FBB017] bg-[#f3ce69] text-white px-4 py-[0.1rem] ${className} `}>{children}</div>
-    {/* <button className='bg-[#FBB017] text-white font-bold'>Second</button> */}
-    </>
-    
-    )
+    <div className={`${className} inline-block rounded border-2 border-[#FBB017] bg-[#f3ce69] text-white px-4 py-[0.1rem]`}>{children}</div>
+  )
 }
 
 export default index
